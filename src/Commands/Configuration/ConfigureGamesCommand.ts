298761159cd2d586171ecfb39a81db466d16f668
Edward Wang <37031713+ewang20027@users.noreply.github.com>
2020-09-18 20:12:04 -0700

    updat dep, node-fetch up one version

---
+++ src/Commands/Configuration/ConfigureGamesCommand.ts
@@ -0,0 +1,42 @@
+import { Command } from "../../Templates/Command/Command";
+import { CommandDetail } from "../../Templates/Command/CommandDetail";
+import { CommandPermission } from "../../Templates/Command/CommandPermission";
+import { Message } from "discord.js";
+import { IRaidGuild } from "../../Templates/IRaidGuild";
+import { ConfigureSectionCommand } from "./ConfigureSectionCommand";
+
+export class ConfigureGameCommand extends Command {
+
+    public constructor() {
+        super(
+            new CommandDetail(
+                "Configure Games Commands",
+                "configuregames",
+                ["configgames"],
+                "Lets you add or removes games to/from the database, either on a server or global level.",
+                ["configuregames"],
+                ["configuregames"],
+                0
+            ),
+            new CommandPermission(
+                ["ADMINISTRATOR"],
+                [],
+                [],
+                [],
+                false
+            ),
+            true, // guild-only command. 
+            false,
+            false,
+            0
+        );
+    }
+
+    public async executeCommand(
+        msg: Message,
+        args: string[],
+        guildDb: IRaidGuild
+    ): Promise<void> {
+        
+    }
+}
